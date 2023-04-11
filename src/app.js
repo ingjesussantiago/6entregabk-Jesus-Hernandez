a404a2e3dfd9309a8e8c5f58041dfd9028dbb16d
Jesus Santiago Hernandez Lopez <ingjesussantiago@gmail.com>
2023-04-10 21:53:14 -0600

    manageproductofs

--- src/app.js
+++ src/app.js
@@ -1,16 +1,29 @@
 import express from "express"
 import { __dirname } from "./utils.js"
+import handlebars from "express-handlebars"
 import inicioRouter from "./router/inicio.router.js"
 
 
 const app = express()
 
 app.use(express.json())
-
-app.use("/",inicioRouter)
-
+app.use(express.urlencoded({ extended: true }))
 
 
-app.listen(8080,(req,res)=>{
-    console.log("Desde el puerto 88080")
+app.use(express.static(__dirname + "public"))
+
+app.engine("handlebars",handlebars.engine())
+app.set("views",__dirname + "/views")
+app.set("view engine", "handlebars")
+
+
+
+
+
+app.use("/", inicioRouter)
+
+
+
+app.listen(8080, (req, res) => {
+    console.log("Desde el puerto 8080")
 })
--- src/router/inicio.router.js
+++ src/router/inicio.router.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router =Router()
 
 router.get("/",(req,res)=>{
-    res.send("pagina inicial")
+    res.send("pagina iniciadl")
 })
 
 export default router
